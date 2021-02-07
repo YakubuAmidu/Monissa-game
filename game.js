4facc130ce3eec17d334bc3cb61c6221910d13e5
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-02-07 02:38:08 -0800

    Initial commit

--- game.js
+++ game.js
@@ -58,3 +58,15 @@ function nextSequence() {
     .fadeIn(100);
   playSound(randomChosenColour);
 }
+
+function animatePress(currentColor) {
+  $('#' + currentColor).addClass('pressed');
+  setTimeout(function () {
+    $('#' + currentColor).removeClass('pressed');
+  }, 100);
+}
+
+function playSound(name) {
+  var audio = new Audio('sounds/' + name + '.mp3');
+  audio.play();
+}
