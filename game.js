c2d5ddfae57e8b2db7b8048ada61b9dca3a9a4e6
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-02-07 02:31:28 -0800

    Initial commit

--- game.js
+++ game.js
@@ -1,3 +1,9 @@
+var buttonColors = ['red', 'yellow', 'green'];
+var gamePattern = [];
+
 function nextSequence() {
-  var randomNumber = Math.random() * 3;
+  var randomNumber = Math.floor(Math.random() * 3);
+  var randomChosenColor = randomNumber;
+  randomChosenColor.buttonColors;
+  randomChosenColor.push(gamePattern);
 }
