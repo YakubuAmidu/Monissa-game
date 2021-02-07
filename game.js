d17190c1f9e96eea94bded014554805750f5153b
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-02-07 02:38:53 -0800

    Inial commit

--- game.js
+++ game.js
@@ -70,3 +70,9 @@ function playSound(name) {
   var audio = new Audio('sounds/' + name + '.mp3');
   audio.play();
 }
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
